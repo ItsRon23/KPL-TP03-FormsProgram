201118e789e40ba577a4cacec6eab0d344c6c4ce
Jospeh <134275356+ItsRon23@users.noreply.github.com>
2024-03-03 18:49:13 +0700

    KPLTP-03

---
+++ TP_MOD_03_1302223083_AaronJoseph/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_MOD_03_1302223083_AaronJoseph
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userName = textBox1.Text;
+            Console.WriteLine(userName);
+            label1.Text = "Halo! " + userName;
+        }
+    }
+}
